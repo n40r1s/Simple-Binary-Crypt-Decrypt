ac0110c119deeb8cbbbd67910b57c26ef04d0453
n40r1s <57507629+n40r1s@users.noreply.github.com>
2021-04-08 08:24:34 +0100

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,19 @@
+def decrypt():
+
+	a_binary_string=input("Entre ton binary\n")
+
+	binary_values = a_binary_string.split()
+
+	ascii_string = ""
+
+	for binary_value in binary_values:
+
+		an_integer = int(binary_value, 2)
+
+		ascii_character = chr(an_integer)
+
+		ascii_string += ascii_character
+
+	print(ascii_string)
+
+decrypt()
