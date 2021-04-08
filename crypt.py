cf32b615c3d2893c5b59b02895b9caec977cf535
n40r1s <57507629+n40r1s@users.noreply.github.com>
2021-04-08 08:55:59 +0100

    Update crypt.py

--- crypt.py
+++ crypt.py
@@ -1,12 +1,12 @@
 def crypt():	
 	a_string = input("Enter the string you want to crypt\n")
-	a_byte_array = bytearray(a_string, "utf8")
+	a_byte_array = bytearray(a_string, "utf 8") #Create bytearray
 	byte_list = []
 	for byte in a_byte_array:
-		binary_representation = bin(byte)
-		byte_list.append(binary_representation)
-	for i in range(0,len(byte_list)):
-		byte_list[i]=byte_list[i].replace('b', '')
+		binary_representation = bin(byte)   #Convert to binary
+		byte_list.append(binary_representation) #Add to list
+	for i in range(0,len(byte_list)):  
+		byte_list[i]=byte_list[i].replace('b', '') 
 		print(byte_list[i]," ",end = '')
 crypt()
 	
